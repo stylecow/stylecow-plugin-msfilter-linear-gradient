74aa282dc2118026242394f3918c36f58491a4e0
oscarotero <oom@oscarotero.com>
2015-06-19 15:53:15 +0200

    changed dependencies

--- index.js
+++ index.js
@@ -20,11 +20,40 @@ module.exports = function (stylecow) {
 				var filter = getFilter(fn.toArray());
 
 				if (filter) {
-					stylecow.utils.addMsFilter(declaration.getParent('Block'), filter);
+					addMsFilter(declaration.getParent('Block'), filter);
 				}
 			}
 		}
 	});
+
+	function addMsFilter (block, filter) {
+		var declaration = block.getChild({
+				type: 'Declaration',
+				name: 'filter',
+				vendor: 'ms'
+			});
+
+		if (!declaration) {
+			return block.push(stylecow.parse('-ms-filter: ' + filter, 'Declaration', 'createMsFilter'));
+		}
+
+		if (declaration.is({string: '-ms-filter: none;'})) {
+			return declaration
+				.get({
+					type: 'Keyword',
+					name: 'none'
+				})
+				.replaceWith((new stylecow.String()).setName(filter));
+		}
+
+		var string = declaration.get('String');
+
+		if (string.name) {
+			string.name += ',' + filter;
+		} else {
+			string.name = filter;
+		}
+	}
 };
 
 function getFilter (params) {
